27b494a5c1fde2a56078d0533ec0b83e8fce4a88
TerlTH <daniilkozyrev90@gmail.com>
2024-01-07 18:48:48 +1000

    добавил 2 окна, привязал базу данных

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.1" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
+
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
+
+
